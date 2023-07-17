11735d58562c60aa7f4ac3459053674eadb7e098
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-18 03:08:02 +0330

    update

--- lib/banner.py
+++ lib/banner.py
@@ -1,15 +1,16 @@
-
+from colorama import Fore as F; from colorama import Style as S
 
 def Banner():
-   loc_st = '''
+  loc_st = f'''
     .@@@@
     @*  @@
      @@@@
       @@ '''
-    ascii_banner = '''
+   
+  ascii_banner = f'''
     _ ___ _   __   ___ __ _____ ___  
     | | _,\ | /__\ / _//  \_   _| __| 
     | | v_/ || \/ | \_| /\ || | | _|  
-    |_|_| |___\__/ \__/_||_||_| |___| {loc_st}'''
+    |_|_| |___\__/ \__/_||_||_| |___| {loc_st}\n'''
 
-    retrun ascii_banner
+  return ascii_banner
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from colorama import Fore as F; from colorama import Style as S
-import sys
+
+from lib.banner import Banner
+banner = Banner()
 
 class style:
-    wrn = '{F.WHITE}[{F.YELLOW}-{F.WHITE}]{F.RESET}' # Warning ascii msg
-    fil = '{F.WHITE}[{F.RED}x{F.WHITE}]{F.RESET}' # Fail ascii msg
-    dne = '{F.WHITE}[{F.GREEN}+{F.WHITE}]{F.RESET}' # Done ascii msg
+    wrn = f'{F.WHITE}[{F.YELLOW}-{F.WHITE}]{F.RESET}' # Warning ascii msg
+    fil = f'{F.WHITE}[{F.RED}x{F.WHITE}]{F.RESET}' # Fail ascii msg
+    dne = f'{F.WHITE}[{F.GREEN}+{F.WHITE}]{F.RESET}' # Done ascii msg
 
 def main():
     while True:
-        ip = str(input('Pv4, IPv6 or Domain name ~$ '))
+        try:
+            ip = str(input('Pv4, IPv6 or Domain name ~$ '))
+        except KeyboardInterrupt:
+            exit()
 
 
 
 if __name__ == '__main__':
+    print(banner)
     main()
