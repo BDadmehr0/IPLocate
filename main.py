4a72846a2ef665e166ca04b937c8e090f9529f41
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-18 02:53:06 +0330

    main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from colorama import Fore as F; from colorama import Style as S
+import sys
 
 class style:
     wrn = '{F.WHITE}[{F.YELLOW}-{F.WHITE}]{F.RESET}' # Warning ascii msg
     fil = '{F.WHITE}[{F.RED}x{F.WHITE}]{F.RESET}' # Fail ascii msg
     dne = '{F.WHITE}[{F.GREEN}+{F.WHITE}]{F.RESET}' # Done ascii msg
+
+def main():
+    while True:
+        ip = str(input('Pv4, IPv6 or Domain name ~$ '))
+
+
+
+if __name__ == '__main__':
+    main()
