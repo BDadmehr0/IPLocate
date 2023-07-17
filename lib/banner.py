d6e4ea6f184d6685da40c9ba32117c56c2f48313
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-18 03:11:35 +0330

    update

--- lib/banner.py
+++ lib/banner.py
@@ -1,6 +1,8 @@
 from colorama import Fore as F; from colorama import Style as S
+from requests import get
 
 def Banner():
+  v = get('https://')
   loc_st = f'''
     .@@@@
     @*  @@
@@ -8,9 +10,9 @@ def Banner():
       @@ '''
    
   ascii_banner = f'''
-    _ ___ _   __   ___ __ _____ ___  
-    | | _,\ | /__\ / _//  \_   _| __| 
-    | | v_/ || \/ | \_| /\ || | | _|  
-    |_|_| |___\__/ \__/_||_||_| |___| {loc_st}\n'''
+ _ ___ _   __   ___ __ _____ ___  .@@@@
+| | _,\ | /__\ / _//  \_   _| __| @*  @@
+| | v_/ || \/ | \_| /\ || | | _|   @@@@
+|_|_| |___\__/ \__/_||_||_| |___|   @@\n V.{v} BDadmehr0 | IP Address Lookup'''
 
   return ascii_banner
