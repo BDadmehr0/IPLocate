ab2c2bdea08058b48bad222dc627c22fd1d7bb71
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-18 02:27:27 +0330

    ascii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from colorama import Fore as F; from colorama import Style as S
 
 class style:
-    wrn = ''
-    fil = ''
-    don = ''
+    wrn = '{F.WHITE}[{F.YELLOW}-{F.WHITE}]{F.RESET}' # Warning ascii msg
+    fil = '{F.WHITE}[{F.RED}x{F.WHITE}]{F.RESET}' # Fail ascii msg
+    dne = '{F.WHITE}[{F.GREEN}+{F.WHITE}]{F.RESET}' # Done ascii msg
