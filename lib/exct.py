0edbd501c551fbc371d710a4d96f089cb472a983
BDadmehr0 <garfoxteam@gmail.com>
2023-09-16 01:38:04 +0330

    update 2.5.0

--- lib/exct.py
+++ lib/exct.py
@@ -9,19 +9,6 @@ class style:
     fil = f'\n{F.WHITE}[{F.RED}x{F.WHITE}]{F.RESET} ' # Fail ascii msg
     dne = f'\n{F.WHITE}[{F.GREEN}+{F.WHITE}]{F.RESET} ' # Done ascii msg
 
-def get_latest_info_file():
-    # Find all files with the pattern 'infoX.json' and get the latest X.
-    existing_files = [f for f in os.listdir() if f.startswith("info") and f.endswith(".json")]
-    if existing_files:
-        last_file = max(existing_files)
-        last_number = int(last_file.split("info")[1].split(".json")[0])
-    else:
-        last_number = 0
-
-    # Create a new file name with an incremented X.
-    new_file_name = f"info{last_number + 1}.json"
-    return new_file_name
-
 def main_APP(ip):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
@@ -40,19 +27,16 @@ def main_APP(ip):
         'ip': f'{ip}',
     }
 
-    response = requests.get('https://api.ipbase.com/v2/info', params=params, headers=headers).text
-    json_object = json.loads(response)
+    response = requests.get('https://api.ipbase.com/v2/info', params=params, headers=headers)
+
+    if response.status_code == 200:
+        return style.dne + f'Get info\n'
+    else:
+        return style.fil + f'Error with get info  {response.status_code}\n'
+
+    json_object = json.loads(response.text)
     json_formatted_str = json.dumps(json_object, indent=2)
 
-    # Get the latest info file name and save data to that file.
-    info_file_name = get_latest_info_file()
-    with open(info_file_name, 'w') as json_file:
+    with open('info.json', 'w') as json_file:
         json_file.write(json_formatted_str)
-    
-    return style.dne + f'Done info saved to {info_file_name}\n'
-
-# Test the main_APP function
-if __name__ == "__main__":
-    ip_address = "YOUR_IP_ADDRESS_HERE"
-    result = main_APP(ip_address)
-    print(result)
+        return style.wrn + f'Info saved to {info_file_name}\n'
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from colorama import Fore as F; from colorama import Style as S
 def main():
     
     try:
-        ip = str(input(f'{F.YELLOW}Pv4, IPv6 or Domain name ~$ '))
+        ip = str(input(f'{F.GREEN}IPv4, IPv6 or Domain name ~$ '))
         print(main_APP(ip=ip))
     except KeyboardInterrupt:
         exit()
